df1a794493a384f615623f2553ebd0ab22d61155
unknown <rajasarmad21@gmail.com>
2019-03-11 00:20:21 -0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import{ RouterModule }from'@angular/router';
+import{FormsModule}from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +17,24 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
     DashboardComponent,
     LoginComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'login', pathMatch: 'full'},
-      { path: 'dashboard', component:DashboardComponent},
+      { path: 'dashboard', 
+      component:DashboardComponent,
+      children:[
+        {
+          path: 'index',
+          component:IndexComponent
+        }
+      ]
+    },
       { path: 'login', component:LoginComponent}
     ])
   ],
